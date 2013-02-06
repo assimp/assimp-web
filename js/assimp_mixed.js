7c0ec5d3cadae6a04c932dcceeaa4fb0a77d63c2
Alexander Gessler <alexander.gessler@gmx.net>
2013-02-06 14:48:53 +0100

    - initial check-in from SVN.

---
+++ js/assimp_mixed.js
@@ -0,0 +1,8 @@
+
+
+// Hover effect for splash images
+// -----------------------------------------------------------------------------
+function roll_over(img_name, img_src)
+{
+   document[img_name].src = img_src;    
+}
